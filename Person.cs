343dc674ef5e93b1bd59e1f46a2e99a1bcf9ccbb
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-10-26 21:44:08 +0530

    [Prajval]Retrieve top 2 records from the list of age less than 60

---
+++ Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LambdaExpressions_Day14
+{
+    class Person
+    {
+        public string name;
+        public string city;
+        public int age;
+        public Person()
+        {
+
+        }
+        public Person(string name, string city, int age)
+        {
+            this.name = name;
+            this.age = age;
+            this.city = city;
+        }
+        public void GettingTopTwo(List<Person> records)
+        {
+            foreach (Person x in records.FindAll(e => (e.age >= 23)).Take(2))
+            {
+                Console.WriteLine(x.age + " " + x.name);
+            }
+        }
+    }
+}
